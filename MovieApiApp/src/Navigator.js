1f65effd03a5b58b10ad8a7d66845e9f40959097
fatemaabdelaziz <fatema_abdelaziz98@hotmail.com>
2019-12-06 23:53:26 +0200

    app initilization and login screen with navigator screen

--- MovieApiApp/src/Navigator.js
+++ MovieApiApp/src/Navigator.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import LoginScreen from './screens/LoginScreen';
-
+import r from './App'
 
 
 const appSwitch = createSwitchNavigator({
   Auth: {
-    screen: LoginScreen
+    screen:LoginScreen
   }
 })
 
