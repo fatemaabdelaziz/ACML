44163bb27e9bc25373d0706d8b024ccc17aa4649
yaramohamedaly <yaraaly9@gmail.com>
2019-12-09 00:50:18 +0200

    edited favorites method to add/remove movie from favorite

--- api/routes/favorites.router.js
+++ api/routes/favorites.router.js
@@ -4,10 +4,20 @@ var admin = require("firebase-admin");
 let db = admin.firestore()
 let doc = db.collection('usersFavorite')
 
-router.post('/add', async (req,res) =>
+router.post('/addRemove', async (req,res) =>
 {
     let uid = req.body.uid;
     let movieId = req.body.movieId;
+    let exists=false;
+    let docId;
+    await doc.where('uid','==',uid).where('movieId','==',movieId).get().then(snapshot =>{
+        snapshot.docs.map(oneElement =>{
+            if(oneElement.data().movieId){
+            docId=oneElement._ref._path.segments[1]
+            exists=true;
+            }
+        })})
+        if(!exists){
     const favorite =await doc.add({
         uid,
         movieId
@@ -18,6 +28,16 @@ router.post('/add', async (req,res) =>
     else{
         return res.send({added:false})
     }
+}
+else{
+    const removed = await doc.doc(docId).delete();
+    if(removed){
+        return res.send({removed:true})
+    }
+    else{
+        return res.send({removed:false})
+    }
+}
 })
 
 module.exports =router;
