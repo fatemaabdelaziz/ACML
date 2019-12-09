3f3ae713cd276a3bab3797eab331cdc8ccf93eb1
yaramohamedaly <yaraaly9@gmail.com>
2019-12-09 02:59:26 +0200

    search screen and added navigations

--- MovieApiApp/src/Navigator.js
+++ MovieApiApp/src/Navigator.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import LoginScreen from './screens/LoginScreen';
+import SearchScreen from './screens/SearchScreen';
 import ResultsScreen from './screens/ResultsScreen';
 
+const HomeStack = createStackNavigator({
+  Search: {
+    screen: SearchScreen
+  },
+  Results: {
+    screen: ResultsScreen
+  }
+})
 
 const appSwitch = createSwitchNavigator({
   Auth: {
-    screen:LoginScreen
+    screen: LoginScreen
   },
-   Results: {
-    screen: ResultsScreen
+  Home: {
+    screen: HomeStack
   }
 })
 
---
+++ MovieApiApp/src/screens/SearchScreen.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { Input, Button } from 'react-native-elements';
+import { View, SafeAreaView, Text, ScrollView } from 'react-native';
+import axios from 'axios';
+
+class SearchScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movie_name: ''
+    }
+  }
+  onSearchPress() {
+    axios.get(`https://api.themoviedb.org/3/search/movie?api_key=8563a98bf612db1aa0f03cc509a04722&language=en-US&query=${this.state.movie_name}&include_adult=true`)
+      .then(res => {
+        console.log(res.data.results)
+        this.props.navigation.navigate('Results', {
+          data: res.data.results
+        })
+      })
+      .catch(err => console.log(err))
+  }
+  render() {
+    return (
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={{ flex: 1, marginLeft: 10, marginRight: 10 }}>
+          <Text style={{ alignSelf: 'center', fontWeight: 'bold', fontSize: 31 }}>PopCorn</Text>
+          <Input
+            label="Enter the movie"
+            labelStyle={{ fontSize: 16, lineHeight: 24, color: 'black' }}
+            containerStyle={{ marginTop: 20 }}
+            onChangeText={(text) => this.setState({ movie_name: text })}
+            value={this.state.movie_name}
+          />
+          <Button
+            type='solid'
+            title='Search'
+            titleStyle={{ fontSize: 16, textTransform: 'uppercase', padding: 15, fontWeight: 'bold' }}
+            buttonStyle={{ borderRadius: 90 }}
+            containerStyle={{ marginTop: 20 }}
+            onPress={this.onSearchPress.bind(this)}
+          />
+          <Button
+            type='solid'
+            title='My Favorites'
+            titleStyle={{ fontSize: 16, textTransform: 'uppercase', padding: 15, fontWeight: 'bold' }}
+            buttonStyle={{ borderRadius: 90, backgroundColor:'red' }}
+            containerStyle={{ marginTop: 20 }}
+            onPress={this.onSearchPress.bind(this)}
+          />
+        </View>
+      </SafeAreaView >
+    )
+  }
+}
+
+export default SearchScreen;
